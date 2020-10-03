14b37bb051dd61cba4112de5fbda7bad61b8283a
CrashAndSideburns <62062835+CrashAndSideburns@users.noreply.github.com>
2020-10-03 00:37:41 -0700

    Removed accidental BOT_TOKEN commit
    Accidentally committed the BOT_TOKEN like an idiot, so I fixed that.

--- cogs/MCStatusCommands.py
+++ cogs/MCStatusCommands.py
@@ -7,8 +7,10 @@ class MCStatusCommands(commands.Cog, name='MC Status Commands'):
     def __init__(self, client):
         self.client = client
 
+    async def cog_check(self, ctx):
+        return str(ctx.guild.id) in database_functions.get_allowed_channels(self.client, ctx)
+
     @commands.command()
-    #@commands.check(is_allowed_channel)
     async def online(self, ctx):
         await ctx.send(mcstatus_functions.playerNames(database_functions.get_ip(self.client, ctx), database_functions.get_port(self.client, ctx)))
 
